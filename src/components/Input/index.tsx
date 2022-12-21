52435e6d15bac423ee6a1949690945c3f64a36df
adautomoises <adautomaleandro@gmail.com>
2022-12-21 11:51:26 -0300

    Componente Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 
-import { Container, Title, InputField } from "./styles";
+import { Container, Title, InputField, Error } from "./styles";
 
 interface Props {
   title: string;
+  type: string;
   name: string;
   register: any;
+  error?: string;
+  placeholder: string;
 }
 
-export function Input({ title, name, register }: Props) {
+export function Input({
+  title,
+  type,
+  name,
+  register,
+  placeholder,
+  error,
+}: Props) {
   return (
     <Container>
       <Title>{title}*</Title>
-      <InputField {...register} name={name}></InputField>
+      <InputField
+        type={type}
+        {...register}
+        name={name}
+        placeholder={placeholder}
+      ></InputField>
+      <Error>{error}</Error>
     </Container>
   );
 }
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -11,10 +11,28 @@ export const Container = styled.main`
 
 export const Title = styled.label`
   font-family: "Montserrat", sans-serif;
-  font-size: 1rem;
-  font-weight: normal;
+  font-size: 14px;
+  font-weight: 500;
   color: var(--black100);
   margin-bottom: 0.25rem;
 `;
 
-export const InputField = styled.input``;
+export const InputField = styled.input`
+  height: 44px;
+
+  font-family: "Montserrat", sans-serif;
+  font-size: 16px;
+  font-weight: normal;
+  text-transform: uppercase;
+
+  padding: 10px 14px;
+  border-radius: 4px;
+  margin-bottom: 5px;
+`;
+
+export const Error = styled.p`
+  font-family: "Montserrat", sans-serif;
+  font-size: 12px;
+  font-weight: normal;
+  text-transform: uppercase;
+`;
