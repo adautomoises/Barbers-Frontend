dc4bfdd81e9ed1aed90ac146cb5f53d3eaacf817
adautomoises <adautomaleandro@gmail.com>
2022-12-26 23:29:48 -0300

    onClick removido

--- src/components/ButtonForm/index.tsx
+++ src/components/ButtonForm/index.tsx
@@ -4,13 +4,12 @@ interface Props {
   type?: "button" | "submit" | "reset" | undefined;
   title: string;
   color: string;
-  onClick: () => void;
 }
 
-export function ButtonForm({ type, title, color, onClick }: Props) {
+export function ButtonForm({ type, title, color }: Props) {
   return (
     <Container color={color}>
-      <Button color={color} type={type} onClick={onClick}>
+      <Button color={color} type={type}>
         {title}
       </Button>
     </Container>
